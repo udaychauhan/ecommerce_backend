ebb9f7bc8c2a632bb127923f3f3988b475fca775
udaychauhan <uday.myplanet@gmail.com>
2023-11-30 21:58:59 -0500

    models refined and code cleaned

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 from pydantic import BaseModel
 import motor.motor_asyncio
 from typing import List
 import os
 from dotenv import load_dotenv
-from models.products import Product, ProductCreate, ProductUpdate
+from models.products import ProductCollection, Product, ProductUpdate
+from bson import ObjectId
+from pymongo import ReturnDocument
 
 app = FastAPI()
 
@@ -22,67 +24,34 @@ database = client[MONGO_DB]
 products_collection = database[MONGO_COLLECTION_PRODUCTS]
 
 @app.post("/products/", response_model=Product)
-async def create_product(product: ProductCreate):
-    # Convert Pydantic model to dict for MongoDB
-    product_dict = product.dict()
+async def createProduct(product: Product = Body(...)):
+    result = await products_collection.insert_one(product.model_dump(by_alias=True, exclude=["id"]))
+    created_product = await products_collection.find_one(
+        {"_id": result.inserted_id}
+    )
+    return created_product
 
-    # Insert the product into MongoDB
-    result = await products_collection.insert_one(product_dict)
-
-    # Assign the inserted document's ID back to the Pydantic model
-    product._get_valueid = str(result.inserted_id)
-
+@app.get("/products/{id}", response_model=Product)
+async def getProduct(id: str):
+    product = await products_collection.find_one({"_id": ObjectId(id)})
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
     return product
 
-@app.get("/products/{product_id}", response_model=Product)
-async def read_product(product_id: str):
-    # Query MongoDB for the product with the specified ID
-    result = await products_collection.find_one({"_id": product_id})
-
-    # If the product is not found, raise an HTTPException
+@app.put("/products/{id}", response_model=Product)
+async def updateProduct(id: str, product_update: ProductUpdate = Body(...)):
+    product_update = {
+        k: v for k, v in product_update.model_dump(by_alias=True).items() if v is not None
+    }
+    result = await products_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": product_update},
+            return_document=ReturnDocument.AFTER
+    )
     if result is None:
         raise HTTPException(status_code=404, detail="Product not found")
+    return result
 
-    # Convert the MongoDB document to a Pydantic model
-    return Product(**result)
-
-@app.put("/products/{product_id}", response_model=Product)
-async def update_product(product_id: str, product_update: ProductUpdate):
-    # Query MongoDB for the product with the specified ID
-    existing_product = await products_collection.find_one({"_id": product_id})
-
-    # If the product is not found, raise an HTTPException
-    if existing_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
-
-    # Update the product fields based on the provided update
-    update_dict = product_update.dict(exclude_unset=True)
-    updated_product = {**existing_product, **update_dict}
-
-    # Update the product in MongoDB
-    await products_collection.update_one({"_id": product_id}, {"$set": updated_product})
-
-    # Convert the updated MongoDB document to a Pydantic model
-    return Product(**updated_product)
-
-@app.delete("/products/{product_id}", response_model=Product)
-async def delete_product(product_id: str):
-    # Query MongoDB for the product with the specified ID
-    result = await products_collection.find_one({"_id": product_id})
-
-    # If the product is not found, raise an HTTPException
-    if result is None:
-        raise HTTPException(status_code=404, detail="Product not found")
-
-    # Delete the product from MongoDB
-    await products_collection.delete_one({"_id": product_id})
-
-    # Convert the deleted MongoDB document to a Pydantic model
-    return Product(**result)
-
-@app.get("/products/", response_model=List[Product])
+@app.get("/products/")
 async def list_products():
-    # Retrieve all products from MongoDB
-    products = await products_collection.find().to_list(length=1000)
-    # Convert the list of MongoDB documents to a list of Pydantic models
-    return [Product(**product) for product in products]
+    return ProductCollection(products=await products_collection.find().to_list(1000))
--- models/products.py
+++ models/products.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from typing_extensions import Annotated
+from pydantic.functional_validators import BeforeValidator
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 # Pydantic model for product
-class ProductCreate(BaseModel):
+class Product(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str
     description: str
     price: float
@@ -11,5 +17,6 @@ class ProductUpdate(BaseModel):
     description: str = None
     price: float = None
 
-class Product(ProductCreate):
-    _id: str
+
+class ProductCollection(BaseModel):
+    products: List[Product]
